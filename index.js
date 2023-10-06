ee4c9e153606b8eec11324f96da78268be12868b
karenhernandez <khernandez0329@gmail.com>
2023-10-06 16:38:41 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"]
+function destructivelyAppendCat(name){
+    cats.push("Ralph")
+}
+    
+console.log(cats)
+console.log(destructivelyAppendCat)
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+}
+console.log(destructivelyPrependCat)
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+
+}
+console.log(destructivelyRemoveLastCat)
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+console.log(destructivelyRemoveFirstCat)
+
+function appendCat(name){
+    const copyOfCats = [...cats.slice(),
+    "Broom"
+    ]
+    return copyOfCats
+}
+console.log(appendCat)
+
+function prependCat(name){
+    const allCats = ["Arnold",
+    ...cats.slice()
+]
+    return allCats
+}
+console.log(prependCat)
+
+function removeLastCat(){
+    const minusCats = [...cats.slice(0, 2)]
+    return minusCats
+}
+console.log(removeLastCat)
+
+function removeFirstCat(){
+    const lessCat = [...cats.slice(1)]
+    return lessCat
+}
+console.log(removeFirstCat)
